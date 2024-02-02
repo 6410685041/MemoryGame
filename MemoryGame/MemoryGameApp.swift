32c6f1a0cdc21ecf407ff109ac3214a688f526b0
6410685041 <6410685041@student.tu.ac.th>
2024-02-02 09:50:33 +0700

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by นางสาวชลิศา ธรรมราช on 2/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
